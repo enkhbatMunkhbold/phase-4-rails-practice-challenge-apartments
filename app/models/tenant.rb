ad6ed5462572ca87f883aa1ea36e624a925783dd
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-08-31 09:00:38 -0700

    Current Status

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,5 +1,6 @@
 class Tenant < ApplicationRecord
   validates :name, presence: true
+  validates :age, numericality: { minimum: 18 }
   has_many :leases
   has_many :apartment, through: :leases
 end
